7e273d4a79deca223ec4fe8de907e8aa9ac21c5e
Hansol Lee <giantsol64@gmail.com>
2019-08-07 23:46:25 +0900

    initial commit

---
+++ android/app/src/main/kotlin/lee/hansol/todo_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package lee.hansol.todo_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
